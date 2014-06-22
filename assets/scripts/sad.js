dc886f3fde2f7c1b67798814596fb28819f60d7b
Joel Califa <jcalifa@gmail.com>
2014-06-22 16:44:05 -0400

    port site into Jekyll

---
+++ assets/scripts/sad.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  var delight = $('h2.delight'),
+      delightCount = 0
+
+  delight.hover(delightBounce)
+  delight.click(delightBounce)
+
+  function delightBounce() {
+    if (!delight.hasClass('animating')) {
+      delight.addClass('animating')
+      setTimeout(function() {
+        delight.removeClass('animating')
+      }, 800)
+      delightCount++;
+    }
+
+    if (delightCount > 4) {
+      setTimeout(function() {
+        $('section.delight p').addClass('shown')
+      }, 500)  
+    }
+  }
+
+})
